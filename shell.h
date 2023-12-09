207cce5c77385232a4911d4b347aa0688f34f0a2
sipho <132463193+siphonzama@users.noreply.github.com>
2023-12-09 18:00:56 +0200

    Create shell.h
    header file created

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+
+
+#endif  /** shell_h header file**/
